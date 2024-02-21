b85c433c024dbf176c7455f5a6af81b7ceddee7a
Qyllz <159618989+Qyllz@users.noreply.github.com>
2024-02-21 21:49:24 +0800

    Add files via upload

---
+++ OOP/Activity3/TestStudent.java
@@ -0,0 +1,28 @@
+package Activity3;
+
+public class TestStudent {
+    public static void main(String args[]) {
+        Student students[] = {
+            new Student("Mathew", "BSCS",  10000.0F),
+            new Student("Mark", "BSIT",  12000.0F),
+            new PartialScholar("Luke", "BSIT",  12000.0F,  0.25F),
+            new PartialScholar("John", "BSIT",  11000.0F,  0.5F),
+        };
+
+        float totalCollection =  0.0F;
+
+        for (int i =  0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
+
+        // compute totalCollection value
+        for (int i =  0; i < students.length; i++) {
+            if (students[i] instanceof PartialScholar)
+                totalCollection += ((PartialScholar) students[i]).getPayableAmount();
+            else if (students[i] instanceof Student)
+                totalCollection += students[i].getTuitionFee();
+        }
+
+        System.out.println("\nTotal Collectible Amount: " + totalCollection);
+    }
+}
