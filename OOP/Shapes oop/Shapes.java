0b379c320a88c08dbd1ad9e90468fb8c167e17b8
Qyllz <159618989+Qyllz@users.noreply.github.com>
2024-03-02 20:46:03 +0800

    Add files via upload

---
+++ OOP/Shapes oop/Shapes.java
@@ -0,0 +1,14 @@
+package objectOrientedPrograming;
+
+public class Shapes {
+	private int numberOfSides;
+	
+	public Shapes(int numberOfSides) {
+		this.numberOfSides = numberOfSides;
+	}
+	
+	public int getNumberOfSides() {
+		return numberOfSides;
+	}
+
+}
